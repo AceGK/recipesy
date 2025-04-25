f873845ebf65afa3204ec8373b8229837f668321
Ace <acekisch@gmail.com>
2025-04-25 13:08:43 -0700

    Update index.js

--- pages/recipes/categories/index.js
+++ pages/recipes/categories/index.js
@@ -2,13 +2,18 @@
 import { getCategories } from '../../../lib/categories';
 import CategoryCard from '../../../components/categoryCard';
 import Header from '../../../components/categoryHeader';
+import Breadcrumbs from '../../../components/breadcrumbs';
 
 export default function CategoriesPage() {
   const { categories } = getCategories();
 
   return (
     <div className='container'>
-      <h1 style={{padding:'2rem 0rem'}}>All Categories</h1>
+      <Breadcrumbs />
+      <div style={{ padding: '2rem 0rem' }}>
+        <h1>All Categories</h1>
+        <p>Browse recipes by type to discover your next favorite meal, drink, or treat.</p>
+      </div>
       <div className="grid">
         {categories.map((category) => (
           <CategoryCard category={category} />
